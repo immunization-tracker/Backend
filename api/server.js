3b649fd34f1ef33b072832e7754efd9623117e85
ghungpham <pham.ghungj@gmail.com>
2019-06-22 00:51:34 -0400

    setting up api

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const server = express()
+
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.send('<h1> API is running!!! </h1>')
+})
+
+module.exports = server;
